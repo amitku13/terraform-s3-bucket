3f8de071f5bee0e96770898301879e9f22136cab
amitku13 <amitkumarbhontele@gmail.com>
2024-11-18 22:56:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,3 @@ resource "aws_s3_bucket" "example" {
     Name = var.bucket_name
   }
 }
-
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
-
-output "bucket_arn" {
-  value = aws_s3_bucket.example.arn
-}
